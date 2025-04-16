925a1302dd82f5b2b45aa60298a46a050c3a7409
remshatariq21 <remshatariq21@gmail.com>
2025-04-16 18:55:54 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 25;
+            string name = "Boy";
+
+            Console.WriteLine("Name : "+name);
+            Console.WriteLine("Age : "+age);
+        }
+    }
+}
