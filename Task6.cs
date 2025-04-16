10d6a96b48b9985b1cad5d02f7493a7780af7d49
remshatariq21 <remshatariq21@gmail.com>
2025-04-17 00:19:34 +0500

    Add files via upload

---
+++ Task6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+namespace TASK6;
+
+class program
+{
+    static string MakeTea()
+    {
+
+        Console.WriteLine("Take a kettle");
+        Console.WriteLine("Add some water and boiled it");
+        Console.WriteLine("Add some tea and suagre in it");
+        Console.WriteLine("Add some milk in it");
+        Console.WriteLine("Boil it well");
+        Console.WriteLine("Tea is ready");
+        return "yes";
+        {
+            string result = MakeTea();
+            Console.WriteLine("Is tea ready ");
+            Console.WriteLine("Result");
+
+        }
+    }
+}
