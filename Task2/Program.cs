b440708ed8fd3948f04db199b3be1b78a97d31f5
remshatariq21 <remshatariq21@gmail.com>
2025-04-16 19:12:18 +0500

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Task2
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("byte:     {0} to {1}", byte.MinValue, byte.MaxValue);
+        Console.WriteLine("sbyte:    {0} to {1}", sbyte.MinValue, sbyte.MaxValue);
+        Console.WriteLine("short:    {0} to {1}", short.MinValue, short.MaxValue);
+        Console.WriteLine("ushort:   {0} to {1}", ushort.MinValue, ushort.MaxValue);
+        Console.WriteLine("int:      {0} to {1}", int.MinValue, int.MaxValue);
+        Console.WriteLine("uint:     {0} to {1}", uint.MinValue, uint.MaxValue);
+        Console.WriteLine("long:     {0} to {1}", long.MinValue, long.MaxValue);
+        Console.WriteLine("ulong:    {0} to {1}", ulong.MinValue, ulong.MaxValue);
+        Console.WriteLine("float:    {0} to {1}", float.MinValue, float.MaxValue);
+        Console.WriteLine("double:   {0} to {1}", double.MinValue, double.MaxValue);
+        Console.WriteLine("decimal:  {0} to {1}", decimal.MinValue, decimal.MaxValue);
+    }
+}
+
+
