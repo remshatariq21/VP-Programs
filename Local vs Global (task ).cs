0efa6d764f25db251d11e065380276c18983fdf8
remshatariq21 <remshatariq21@gmail.com>
2025-04-20 20:58:53 +0500

    Add files via upload

---
+++ Local vs Global (task ).cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    // Global variable
+    static int globalNumber = 100;
+
+    static void Main()
+    {
+        // Local variable
+        int localNumber = 50;
+
+        Console.WriteLine("Global Number: " + globalNumber);
+        Console.WriteLine("Local Number: " + localNumber);
+    }
+}
+
