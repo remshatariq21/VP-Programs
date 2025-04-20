54cef8ea60f395af2bc41755b7d45c8ee667a88d
remshatariq21 <remshatariq21@gmail.com>
2025-04-20 22:32:09 +0500

    Add files via upload

---
+++ Task1/constant vs readonly.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+  
+    const int constantValue = 10;
+
+ 
+    readonly int readonlyValue;
+
+    public Program()
+    {
+      
+        readonlyValue = 20;
+    }
+
+    static void Main()
+    {
+      
+        Program program = new Program();
+
+        Console.WriteLine("Constant Value: " + constantValue);
+        Console.WriteLine("Readonly Value: " + program.readonlyValue);
+
+       
+    }
+}
