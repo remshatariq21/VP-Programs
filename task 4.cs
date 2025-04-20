2a89935a23649248adc2376743c1ee6f823dc199
remshatariq21 <remshatariq21@gmail.com>
2025-04-20 20:39:32 +0500

    Add files via upload

---
+++ task 4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection.Metadata;
+namespace Task4;
+class program
+{
+    static void Main(string[] args)
+    {
+        string[] names = ["Ali","Ahmed","Bilal","imran","Hanif"];
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
+
